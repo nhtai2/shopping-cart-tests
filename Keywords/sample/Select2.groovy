82a70e69c5f32718cb67e9bd394113c9768be6af
Nguyen Huu, Tai <nhtai2@tma.com.vn>
2024-02-07 12:36:02 +0700

    Commit 1 TC-002

--- Keywords/sample/Select2.groovy
+++ Keywords/sample/Select2.groovy
@@ -14,6 +14,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import org.openqa.selenium.JavascriptExecutor
 import groovy.transform.CompileStatic
 
+// Comment
 public class Select2 {
 
 	/**
