0e1dcb5eb4a49c7baa45c8f62a5f834d839a44d1
Nguyen Huu, Tai <nhtai2@tma.com.vn>
2024-02-07 09:18:12 +0700

    commit pr-7

--- Keywords/sample/BlockUIDismissed.groovy
+++ Keywords/sample/BlockUIDismissed.groovy
@@ -18,6 +18,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 import internal.GlobalVariable
 
+//comment
 public class BlockUIDismissed {
 	def static void WaitBlockUIDismissed(){
 		TestObject icoBlockUI = findTestObject('Pages/Checkout page/blockUI')
